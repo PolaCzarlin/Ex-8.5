7d2e50071affe11dfc40f402312043d9bc6f3c49
Aga Sipowicz <agns22@wp.pl>
2017-06-14 19:35:59 +0200

    Triangle Area Exercise

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+var side = prompt('Wartość boku');
+var height = prompt('Wysokość');
+var triangleArea= ((side*height)/2);
+
+alert('Pole trójkąta wynosi: ' + triangleArea);
+console.log('Triangle field with base side: ' + side + ' and height: ' + height + ' is equal to: ' + triangleArea);
